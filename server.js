fd04e84e2638e0af191dff575f16902a2bbb67b9
kolates <sandipkolate2010@gmail.com>
2019-06-01 01:35:43 +0530

    .

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ res.sendFile(path.join(__dirname+'/dist/issuetracker/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
